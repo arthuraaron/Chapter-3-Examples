4493c03a9850e8b5792b658bcec0196995532d57
aaron.d.arthur <aaron.d.arthur@CLC-LAB209-10.TCSD.tooelesd.org>
2018-09-12 12:53:10 -0600

    Edit to compute area

--- src/aaronarthur/Ch3Examples/ComputeArea.java
+++ src/aaronarthur/Ch3Examples/ComputeArea.java
@@ -9,9 +9,12 @@ public class ComputeArea {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		// enter the radius of a circle
+		
+		// Variables
 		double radius;
-		double pi;
 		double area;
+		// Constants
+		double PI = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679; 
 		
 		// assign a value to radius
 		System.out.print("Enter the radius: ");
@@ -21,7 +24,7 @@ public class ComputeArea {
 		// compute the area
 		//pi * radius * radius
 		
-		area = radius*radius*3.141592;
+		area = radius*radius*PI;
 		
 		// print the result
 		System.out.println("Area: " + area);
